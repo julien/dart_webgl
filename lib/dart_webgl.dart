9bc5eb7bf2b4dfeef05ad97d76e59a26bb79fab9
Julien Castelain <julienc@mbp.local>
2014-06-29 00:05:04 +0200

    removing unused stuff2

--- lib/dart_webgl.dart
+++ lib/dart_webgl.dart
@@ -3,9 +3,13 @@ library dart_webgl;
 import 'dart:math';
 import 'dart:typed_data';
 
+
 part 'doubly_linked_list.dart';
 part 'particle.dart';
 part 'particle_pool.dart';
-part 'vec2.dart';
+
+// complety useless right now 
+// (and since we're using vector_math even more)
+part 'vec2.dart'; 
 
 void main() { }
